3ae861c098dc4e85a1c5fc270561a936b77b2893
abhinavdayal <abhinav.dayal@hotmail.com>
2020-08-01 13:58:07 +0530

    t

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ mean, net = prepareNet()
 class home(Resource):
 	#form = ImageForm()
 	def options (self):
-		return {'Allow' : 'PUT' }, 200, {'Access-Control-Allow-Origin': '*', 'Access-Control-Allow-Methods' : 'PUT,GET',  "Access-Control-Allow-Headers": "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With" }
+		return {'Allow' : 'PUT' }, 200, {'Access-Control-Allow-Origin': '*', 'Access-Control-Allow-Methods' : 'PUT,GET',  "Access-Control-Allow-Headers": "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With, Access-Control-Allow-Origin, Access-Control-Allow-Methods" }
 
 	def post(self):
 		parser = reqparse.RequestParser()
